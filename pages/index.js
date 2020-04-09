66bc5410176666b18122e85e936ba41611b5eac8
吴琼 <wuqiongaia@163.com>
2020-04-09 20:58:38 +0800

    创建第一个插件

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>第一个组建d</h1>
+            </div>
+        )
+    }
+}
